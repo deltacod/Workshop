d9fcaa99ddc4b843a9ce9f06c04feed61dd99ff3
fustyles <34397289+fustyles@users.noreply.github.com>
2019-12-19 19:54:50 +0800

    Add files via upload

--- 2019.12.20_esp32-cam/ESP32-CAM_coco-ssd/ESP32-CAM_coco-ssd.ino
+++ 2019.12.20_esp32-cam/ESP32-CAM_coco-ssd/ESP32-CAM_coco-ssd.ino
@@ -874,9 +874,172 @@ static const char PROGMEM INDEX_HTML[] = R"rawliteral(<!doctype html>
             </div>
         </section>
         <div id="result"><div>
+        
         <script>
-          document.addEventListener('DOMContentLoaded',function(){function b(B){let C;switch(B.type){case'checkbox':C=B.checked?1:0;break;case'range':case'select-one':C=B.value;break;case'button':case'submit':C='1';break;default:return;}const D=`${c}/control?var=${B.id}&val=${C}`;fetch(D).then(E=>{console.log(`request to ${D} finished, status: ${E.status}`)})}var c=document.location.origin;const e=B=>{B.classList.add('hidden')},f=B=>{B.classList.remove('hidden')},g=B=>{B.classList.add('disabled'),B.disabled=!0},h=B=>{B.classList.remove('disabled'),B.disabled=!1},i=(B,C,D)=>{D=!(null!=D)||D;let E;'checkbox'===B.type?(E=B.checked,C=!!C,B.checked=C):(E=B.value,B.value=C),D&&E!==C?b(B):!D&&('aec'===B.id?C?e(v):f(v):'agc'===B.id?C?(f(t),e(s)):(e(t),f(s)):'awb_gain'===B.id?C?f(x):e(x):'face_recognize'===B.id&&(C?h(n):g(n)))};document.querySelectorAll('.close').forEach(B=>{B.onclick=()=>{e(B.parentNode)}}),fetch(`${c}/status`).then(function(B){return B.json()}).then(function(B){document.querySelectorAll('.default-action').forEach(C=>{i(C,B[C.id],!1)})});const j=document.getElementById('stream'),k=document.getElementById('stream-container'),l=document.getElementById('get-still'),m=document.getElementById('toggle-stream'),n=document.getElementById('face_enroll'),o=document.getElementById('close-stream'),p=()=>{window.stop(),m.innerHTML='Start Stream'},q=()=>{j.src=`${c+':81'}/stream`,f(k),m.innerHTML='Stop Stream'};l.onclick=()=>{p(),j.src=`${c}/capture?_cb=${Date.now()}`,f(k)},o.onclick=()=>{p(),e(k)},m.onclick=()=>{const B='Stop Stream'===m.innerHTML;B?p():q()},n.onclick=()=>{b(n)},document.querySelectorAll('.default-action').forEach(B=>{B.onchange=()=>b(B)});const r=document.getElementById('agc'),s=document.getElementById('agc_gain-group'),t=document.getElementById('gainceiling-group');r.onchange=()=>{b(r),r.checked?(f(t),e(s)):(e(t),f(s))};const u=document.getElementById('aec'),v=document.getElementById('aec_value-group');u.onchange=()=>{b(u),u.checked?e(v):f(v)};const w=document.getElementById('awb_gain'),x=document.getElementById('wb_mode-group');w.onchange=()=>{b(w),w.checked?f(x):e(x)};const y=document.getElementById('face_detect'),z=document.getElementById('face_recognize'),A=document.getElementById('framesize');A.onchange=()=>{b(A),5<A.value&&(i(y,!1),i(z,!1))},y.onchange=()=>{return 5<A.value?(alert('Please select CIF or lower resolution before enabling this feature!'),void i(y,!1)):void(b(y),!y.checked&&(g(n),i(z,!1)))},z.onchange=()=>{return 5<A.value?(alert('Please select CIF or lower resolution before enabling this feature!'),void i(z,!1)):void(b(z),z.checked?(h(n),i(y,!0)):g(n))}});
+          document.addEventListener('DOMContentLoaded', function (event) {
+            var baseHost = document.location.origin
+            var streamUrl = baseHost + ':81'
+            const hide = el => {
+              el.classList.add('hidden')
+            }
+            const show = el => {
+              el.classList.remove('hidden')
+            }
+            const disable = el => {
+              el.classList.add('disabled')
+              el.disabled = true
+            }
+            const enable = el => {
+              el.classList.remove('disabled')
+              el.disabled = false
+            }
+            const updateValue = (el, value, updateRemote) => {
+              updateRemote = updateRemote == null ? true : updateRemote
+              let initialValue
+              if (el.type === 'checkbox') {
+                initialValue = el.checked
+                value = !!value
+                el.checked = value
+              } else {
+                initialValue = el.value
+                el.value = value
+              }
+              if (updateRemote && initialValue !== value) {
+                updateConfig(el);
+              } else if(!updateRemote){
+                if(el.id === "aec"){
+                  value ? hide(exposure) : show(exposure)
+                } else if(el.id === "agc"){
+                  if (value) {
+                    show(gainCeiling)
+                    hide(agcGain)
+                  } else {
+                    hide(gainCeiling)
+                    show(agcGain)
+                  }
+                } else if(el.id === "awb_gain"){
+                  value ? show(wb) : hide(wb)
+                }
+              }
+            }
+            function updateConfig (el) {
+              let value
+              switch (el.type) {
+                case 'checkbox':
+                  value = el.checked ? 1 : 0
+                  break
+                case 'range':
+                case 'select-one':
+                  value = el.value
+                  break
+                case 'button':
+                case 'submit':
+                  value = '1'
+                  break
+                default:
+                  return
+              }
+              const query = `${baseHost}/control?var=${el.id}&val=${value}`
+              fetch(query)
+                .then(response => {
+                  console.log(`request to ${query} finished, status: ${response.status}`)
+                })
+            }
+            document
+              .querySelectorAll('.close')
+              .forEach(el => {
+                el.onclick = () => {
+                  hide(el.parentNode)
+                }
+              })
+            // read initial values
+            fetch(`${baseHost}/status`)
+              .then(function (response) {
+                return response.json()
+              })
+              .then(function (state) {
+                document
+                  .querySelectorAll('.default-action')
+                  .forEach(el => {
+                    updateValue(el, state[el.id], false)
+                  })
+              })
+            const view = document.getElementById('stream')
+            const viewContainer = document.getElementById('stream-container')
+            const stillButton = document.getElementById('get-still')
+            const streamButton = document.getElementById('toggle-stream')
+            const closeButton = document.getElementById('close-stream')
+            const stopStream = () => {
+              //window.stop();
+              view.src="";
+              streamButton.innerHTML = 'Start Stream'
+            }
+            const startStream = () => {
+              view.src = `${streamUrl}/stream`
+              show(viewContainer)
+              streamButton.innerHTML = 'Stop Stream'
+            }
+            // Attach actions to buttons
+            stillButton.onclick = () => {
+              stopStream()
+              try{
+                view.src = `${baseHost}/capture?_cb=${Date.now()}`
+              }
+              catch(e) {
+                view.src = `${baseHost}/capture?_cb=${Date.now()}`  
+              }
+              show(viewContainer)
+            }
+            closeButton.onclick = () => {
+              stopStream()
+              hide(viewContainer)
+            }
+            streamButton.onclick = () => {
+              const streamEnabled = streamButton.innerHTML === 'Stop Stream'
+              if (streamEnabled) {
+                stopStream()
+              } else {
+                startStream()
+              }
+            }
+            // Attach default on change action
+            document
+              .querySelectorAll('.default-action')
+              .forEach(el => {
+                el.onchange = () => updateConfig(el)
+              })
+            // Custom actions
+            // Gain
+            const agc = document.getElementById('agc')
+            const agcGain = document.getElementById('agc_gain-group')
+            const gainCeiling = document.getElementById('gainceiling-group')
+            agc.onchange = () => {
+              updateConfig(agc)
+              if (agc.checked) {
+                show(gainCeiling)
+                hide(agcGain)
+              } else {
+                hide(gainCeiling)
+                show(agcGain)
+              }
+            }
+            // Exposure
+            const aec = document.getElementById('aec')
+            const exposure = document.getElementById('aec_value-group')
+            aec.onchange = () => {
+              updateConfig(aec)
+              aec.checked ? hide(exposure) : show(exposure)
+            }
+            // AWB
+            const awb = document.getElementById('awb_gain')
+            const wb = document.getElementById('wb_mode-group')
+            awb.onchange = () => {
+              updateConfig(awb)
+              awb.checked ? show(wb) : hide(wb)
+            }
+          })
         </script>
+        
     <!-- Object Detection -->    
     <script>
     var getStill = document.getElementById('get-still');
