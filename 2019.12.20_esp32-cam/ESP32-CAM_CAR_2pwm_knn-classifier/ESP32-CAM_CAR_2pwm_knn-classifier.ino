c6cf1a1757a6aeeb17b2d0257b1d0eb5297e9b3f
fustyles <34397289+fustyles@users.noreply.github.com>
2019-12-22 14:45:58 +0800

    Update ESP32-CAM_CAR_2pwm_knn-classifier.ino

--- 2019.12.20_esp32-cam/ESP32-CAM_CAR_2pwm_knn-classifier/ESP32-CAM_CAR_2pwm_knn-classifier.ino
+++ 2019.12.20_esp32-cam/ESP32-CAM_CAR_2pwm_knn-classifier/ESP32-CAM_CAR_2pwm_knn-classifier.ino
@@ -15,10 +15,10 @@ http://192.168.xxx.xxx/control?ip
 http://192.168.xxx.xxx/control?mac
 http://192.168.xxx.xxx/control?restart
 http://192.168.xxx.xxx/control?resetwifi=ssid;password
-http://192.168.xxx.xxx/control?flash=value        //vale= 0~255
+http://192.168.xxx.xxx/control?flash=value        //value= 0~255
 
-http://192.168.xxx.xxx/control?speedL=value  //左輪車速 vale= 0~255 
-http://192.168.xxx.xxx/control?speedR=value  //右輪車速 vale= 0~255
+http://192.168.xxx.xxx/control?speedL=value  //左輪車速 value= 0~255 
+http://192.168.xxx.xxx/control?speedR=value  //右輪車速 value= 0~255
 http://192.168.xxx.xxx/control?decelerate=value  //轉彎減速為原速的比例% vale= 0~100
 http://192.168.xxx.xxx/control?car=1  //前進
 http://192.168.xxx.xxx/control?car=2  //左轉
